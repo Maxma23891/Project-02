2988afce14938ae0495a1d9fab698401e0884892
WinSama <Nithirat7671@gmail.com>
2024-10-05 19:48:02 +0700

    Respawn

--- Gme porjcet/Assets/Code/CameraFollow.cs
+++ Gme porjcet/Assets/Code/CameraFollow.cs
@@ -4,12 +4,11 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform target;
     public float CameraSpeed;
-    public float YoffSet;
-
+    public float YOffset;
     // Update is called once per frame
     void Update()
     {
-        Vector3 newpos = new Vector3(target.position.x, target.position.y + YoffSet, -10f);
-        transform.position = Vector3.Slerp(transform.position, newpos, CameraSpeed * Time.deltaTime);
+        Vector3 newPos = new Vector3(target.position.x, target.position.y + YOffset, -10f);  //YOffset is set the Y
+        transform.position = Vector3.Slerp(transform.position, newPos, CameraSpeed * Time.deltaTime); // Slerp Make it look smoo
     }
 }
--- Gme porjcet/Assets/Code/Respawn.cs
+++ Gme porjcet/Assets/Code/Respawn.cs
@@ -1,13 +1,46 @@
+using System.Collections;
 using UnityEngine;
 
 public class Respawn : MonoBehaviour
 {
+    Vector2 startpos;
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     private void Start()
     {
-        
+        startpos = transform.position;
     }
-    private void Update()
+    void Update()
     {
-        
+
     }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.CompareTag("Obsticle"))
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        StartCoroutine(Delaytime(1f));
+    }
+
+   IEnumerator Delaytime(float Duration)
+    {
+        rb.simulated = false;
+        transform.localScale = new Vector3 (0f, 0f, 0f);
+        transform.position = startpos;
+        yield return new WaitForSeconds(Duration);
+        transform.localScale = new Vector3(1f, 1f, 1f);
+        rb.simulated = true;
+    }
+
+
 }
