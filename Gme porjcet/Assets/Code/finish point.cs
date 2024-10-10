68428a254c8589cc25d4045e2f8800aade4afc65
WinSama <Nithirat7671@gmail.com>
2024-10-10 11:54:25 +0700

    ,

--- Gme porjcet/Assets/Code/finish point.cs
+++ Gme porjcet/Assets/Code/finish point.cs
@@ -4,11 +4,20 @@ using UnityEngine.SceneManagement;
 
 public class finishpoint : MonoBehaviour
 {
+    [SerializeField]bool goNextLevel;
+    [SerializeField] string LevelName;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            
+            if (goNextLevel)
+            {
+                SceneSetting.instance.NextLevel();
+            }
+            else
+            {
+                SceneSetting.instance.SceneLoad(LevelName);
+            }
         }
     }
 }
